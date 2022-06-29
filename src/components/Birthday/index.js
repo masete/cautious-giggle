24fc2128377da036aa585e36819c92f143009f49
masete <nicholasmasete72@gmail.com>
2022-06-29 19:32:06 +0300

    Powered the clear all button using useState

--- src/components/Birthday/index.js
+++ src/components/Birthday/index.js
@@ -1,43 +1,19 @@
-import './index.scss';
-import React from 'react';
-import { useState } from 'react';
-import ReactDOM from 'react-dom';
-// import { useState } from 'react';
-import infor from './data';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+import './index.css';
 
-
-const Birthday = () =>{
-
-    const [people, setPeople] = useState(infor)
-    return(
-        <main>
-            <section className='container'>
-                <h3>Birthdays today</h3>
-                {infor.map((data) => {
-                    return <Person key={data.id} data={data}></Person>
-                })}
-
-                <button onClick={() => setPeople([])}>Clear List</button>
-                
-            </section>
-        </main>
-    );
-}
-
-const Person = (props) =>{
-    const { name, age, image } = props.data;
-    return(
-        <article className='data'>
-            
-            <img src={image} alt='nae' />
-            <div>
-                <h4>{name}</h4>
-                <p>{age} Years</p>
-            </div>
-            
-        </article>
-    )
-}
-;
+function Birthday() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
+};
 
 export default Birthday
