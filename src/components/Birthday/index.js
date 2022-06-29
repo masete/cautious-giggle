0cbd53175b627a7a909e5d896e2e0c89526919b7
masete <nicholasmasete72@gmail.com>
2022-06-29 10:46:29 +0300

    added props

--- src/components/Birthday/index.js
+++ src/components/Birthday/index.js
@@ -1,12 +1,16 @@
 import './index.scss'
+// import { useState } from 'react';
 import data from './data'
 
-const Birthday = () =>{
+const Birthday = (props) =>{
+    const people = () =>{
+        {props.name} = props;
+    }
     return(
         <main>
             <div className='container'>
                 <h2>Todays birthday</h2>
-                People = {data}
+                 <h5> {data.name} </h5>
                 <button>Clear All</button>
             </div>
         </main>
